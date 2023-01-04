ee0172e651f8141bbb860eb880bb95a5cd0d1f8c
den4k-pr <106625230+den4k-pr@users.noreply.github.com>
2023-01-04 04:19:23 +0100

    Update planesService.js

--- src/store/services/planesService.js
+++ src/store/services/planesService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const getPlanes = async () => {
-    const planes = await axios.get('/api/planes');
+    const planes = await axios.get('https://rudenko-back.up.railway.app/api/planes');
 
     return planes.data;
 }
