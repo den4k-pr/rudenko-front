9e55a4ac51384aa2b092ed398ec12afa16b294cd
den4k-pr <106625230+den4k-pr@users.noreply.github.com>
2023-01-07 12:25:26 +0100

    Update paintings.jsx

--- src/pages/paintings.jsx
+++ src/pages/paintings.jsx
@@ -5,6 +5,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { getPlanes } from '../store/planes/planesSlice';
 import { getCategories } from '../store/categories/categoriesSlice';
 import Footer from '../partials/footer';
+import Pr from './pr';
 
 const Paintings = () => {
     const [choseType, setChoseType] = useState()
@@ -38,22 +39,7 @@ const Paintings = () => {
                     <div className="testBlok">
                     {
                     planes && planes.map(obj => (
-                        <Link to={"/product/" + obj._id} key={obj._id} className={ choseType === obj.category || choseBoolean === true ? "main-slider-slide" : "hide"}>
-                            <img className="main-slider-slide_img" src={obj.image} alt="" />
-                            <div className="main-slider-textLeft">
-                            <h3 className="main-slider-slide_name">{obj.title}</h3>
-                            <p className="main-slider-slide_fio">{obj.name}</p>
-                            <p className="main-slider-slide_data">{obj.materials}</p>
-                            <p className="main-slider-slide_size">{obj.size}</p>
-                            <div className="main-slider-slide__bottom">
-                                <p className="main-slider-slide__bottom_price">&#36; {obj.price}</p>
-                                <div className="main-slider-slide__bottom-links">
-                                    <div className="main-slider-slide__bottom-ikons-box1"><img className="main-slider-slide__bottom-ikons-ikon" src="./images/product-searck.png" alt="" /></div>
-                                    <div className="main-slider-slide__bottom-ikons-box2"><img className="main-slider-slide__bottom-ikons-ikon" src="./images/product-busket.png" alt="" /></div>
-                                </div>
-                            </div>
-                            </div>
-                        </Link>
+                        <Pr ID={obj._id} title={obj.title} name={obj.name} materials={obj.materials} size={obj.size} price={obj.price}/>
                     ))
                 }
                     </div>
