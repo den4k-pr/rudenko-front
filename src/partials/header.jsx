731f1552ea9bf43dde670c5d12d3a689ab51bb78
den4k-pr <106625230+den4k-pr@users.noreply.github.com>
2023-01-07 10:30:00 +0100

    Update header.jsx

--- src/partials/header.jsx
+++ src/partials/header.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import '../styles/header.css';
 import { useSelector, useDispatch } from 'react-redux';
 import CartItem from '../partials/cartItem';
