97e001a833f2d115793fa0565191ff68312fb005
den4k-pr <106625230+den4k-pr@users.noreply.github.com>
2023-01-07 08:39:23 +0100

    Update busket-form.jsx

--- src/partials/adminPart/busket-form.jsx
+++ src/partials/adminPart/busket-form.jsx
@@ -47,7 +47,59 @@ const BusketForm = () => {
                     <input name="client_fullName" type="text" placeholder="Write your Full name" required/>
                     <input name="client_email" type="email" placeholder="Write your email" required/>
                     <input name="client_phone" type="text" placeholder="Write your phone" required/>
-                    <select name="client_state" id=""></select>
+                    <select className="states" name="client_state" id="">
+                        <option>Choose State</option>
+                        <option>Alabama</option>
+                        <option>Alaska</option>
+                        <option>Arizona</option>
+                        <option>Arkansas</option>
+                        <option>California</option>
+                        <option>Colorado</option>
+                        <option>Connecticut</option>
+                        <option>Delaware</option>
+                        <option>Florida</option>
+                        <option>Georgia</option>
+                        <option>Hawaii</option>
+                        <option>Idaho</option>
+                        <option>Illinois</option>
+                        <option>Indiana</option>
+                        <option>Iowa</option>
+                        <option>Kansas</option>
+                        <option>Kentucky</option>
+                        <option>Louisiana</option>
+                        <option>Maine</option>
+                        <option>Maryland</option>
+                        <option>Massachusetts</option>
+                        <option>Michigan</option>
+                        <option>Minnesota</option>
+                        <option>Mississippi</option>
+                        <option>Missouri</option>
+                        <option>Montana</option>
+                        <option>Nebraska</option>
+                        <option>Nevada</option>
+                        <option>New Hampshire</option>
+                        <option>New Jersey</option>
+                        <option>New Mexico</option>
+                        <option>New York</option>
+                        <option>North Carolina</option>
+                        <option>North Dakota</option>
+                        <option>Ohio</option>
+                        <option>Oklahoma</option>
+                        <option>Oregon</option>
+                        <option>Pennsylvania</option>
+                        <option>Rhode Island</option>
+                        <option>South Carolina</option>
+                        <option>South Dakota</option>
+                        <option>Tennessee</option>
+                        <option>Texas</option>
+                        <option>Utah</option>
+                        <option>Vermont</option>
+                        <option>Virginia</option>
+                        <option>Washington</option>
+                        <option>West Virginia</option>
+                        <option>Wisconsin</option>
+                        <option>Wyoming</option>
+                    </select>
                     <input name="client_addresses" type="text" placeholder="Write your addresses" required/>
                     <input name="client_index" type="number" placeholder="Write your index" required/>
                     <textarea name="client_properties" type="text" placeholder="Write your properties" required></textarea>
