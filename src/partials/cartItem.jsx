f9b0aee01d2850a521f93624e1fdd92169fb48a9
den4k-pr <106625230+den4k-pr@users.noreply.github.com>
2023-01-12 21:19:31 +0100

    Update cartItem.jsx

--- src/partials/cartItem.jsx
+++ src/partials/cartItem.jsx
@@ -1,13 +1,19 @@
 import React, {useState} from "react";
 import { useDispatch } from "react-redux";
+import { clearItems } from '../store/cart/cartSlice';
 
 const CartItem = ({id, title, price, planeImage}) => {
-    const [delItem, setDelItem] = useState(true);
+
+    const onClickClear = () => {
+        if (window.confirm('Clear buscket?')){
+            dispatch(clearItems())
+        }
+    }
     
     console.log(id)
     
     return (
-        <div className={delItem === true ? "bascet-content-product" : "hide"}>
+        <div className="bascet-content-product">
             <div className="bascet-content-product-body">
                 <img src={planeImage} alt="" className="bascet-content-product-body-img" />
                 <div className="bascet-content-product-body-box">
@@ -15,7 +21,7 @@ const CartItem = ({id, title, price, planeImage}) => {
                     <p className="bascet-content-product-body-price">{price} $</p>
                 </div>
             </div>
-            <div onClick={() => setDelItem(false)} className="bascet-content-product-del" ></div>
+            <div onClick={onClickClear} className="bascet-content-product-del" ></div>
         </div>
     )
 }
